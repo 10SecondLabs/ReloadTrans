cee0b6b09fbb7ad51323b70e9bae2d4a5754abf5
Mike Woodsmall <mwoodsmall@dyknow.com>
2013-01-26 00:08:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReloadTransportation::Application.config.secret_token = '1501cfe660e7ff3ea81180d28d3569dcaad59012e720a451a26167682dbda0dffb36ce32c75065735b1f95ee7efdea74cbf849643dd4fac490a9cf5ab77649c1'
